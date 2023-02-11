5eb2638379843ac6e009bc9ccfbbefa29576d648
King Andrei Caburnay <kingandrei.caburnay@gmail.com>
2023-02-11 18:08:18 +0800

    Initial MyApplication project

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.util.Log;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity{
+
+    private static final String TAG = "MainActivity";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v(TAG, "This is a verbose tag. ");
+        Log.d(TAG, "This is a debug log. ");
+        Log.i(TAG, "This is a info log. ");
+        Log.w(TAG, "This is a warn log. ");
+        Log.e(TAG, "This is a error log. ");
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button Clicked");
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
