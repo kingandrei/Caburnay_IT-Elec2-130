8fe5b2b27089d458779b31e29b6084028e37e798
Admin <kingandrei.cabrunay@gmail.com>
2023-03-11 19:06:56 +0800

    Fragments

--- app/src/main/java/com/example/myapplication/MainFragment.java
+++ app/src/main/java/com/example/myapplication/MainFragment.java
@@ -1,14 +1,45 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainFragment extends AppCompatActivity {
+
+    Button button1, button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_fragment);
+
+       button1 = (Button) findViewById(R.id.firstfragment);
+        button2 = (Button) findViewById(R.layout.fragment_b);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FragmentA());
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FragmentB());
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
